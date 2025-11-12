6cea2aa9f5640e0e6203f3da9cc793952bea937c
Rutvi Gajipara <162660373+rutvipatel122@users.noreply.github.com>
2025-11-12 13:56:32 -0500

    Update streamlit_app.py
    Fixed error in cxn = st.connection to cxn = st.connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your smoothie will be: ", name_on_order)
 
-cnx = st.connection("Snowflake")
+cnx = st.connections("Snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
